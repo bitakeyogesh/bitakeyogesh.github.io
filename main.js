792fd7fce624036466379d1e4c449b23147f1a14
ybitake <ybitake@paypal.com>
2022-06-18 22:59:26 +0530

    revamp the website from scratch

---
+++ main.js
@@ -0,0 +1,45 @@
+window.onload = function () {
+    Particles.init({
+        selector: '.background',
+        color: '#75A5B7',
+        maxParticles: 130,
+        connectParticles: true,
+        responsive: [{
+            breakpoint: 768,
+            options: {
+                maxParticles: 80
+            }
+        }, {
+            breakpoint: 375,
+            options: {
+                maxParticles: 50
+            }
+        }]
+    });
+};
+
+const navBarButton = document.querySelector('.navbar-toggler');
+const navbarCollapseElement = document.querySelector('.navbar-collapse');
+navBarButton.addEventListener('click', () => {
+    navbarCollapseElement.classList.toggle("show");
+});
+
+
+const navLinks = document.getElementsByClassName('nav-link');
+const sectionContainers = document.getElementsByClassName('section-container');
+
+for (let link of navLinks) {
+    link.addEventListener('click', (event) => {
+        const activeNavItem = event.target.id;
+        for (let link of navLinks) {
+            link.classList.remove("activeLink");
+        }
+        link.classList.toggle("activeLink");
+        for (let container of sectionContainers) {
+            container.classList.remove("active");
+            if (container.className.includes(`${activeNavItem}-section`)) {
+                container.classList.add("active");
+            }
+        }
+    })
+}
